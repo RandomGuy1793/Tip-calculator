682cfdd913da9c5c81e15dc115a1339ac120b31b
Faisal Alam <faisal.alam.1999@gmail.com>
2021-11-07 15:40:02 +0530

    design tweaks in script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-var box = document.querySelector("#checkbox");
-var split = document.querySelector("#split");
+let box = document.querySelector("#checkbox");
+let split = document.querySelector("#split");
+let calcButton = document.querySelector(".calc");
+let splitButton = document.querySelector(".check");
 
-function calculate() {
-    var bill = parseFloat(document.querySelector("#bill").value);
-    var tip = parseFloat(document.querySelector("#tip").value);
-    if (isNaN(bill)) alerting(document.querySelector(".bill-alert"))
-    if (isNaN(tip)) alerting(document.querySelector(".tip-alert"))
-    var total = document.querySelector("#pay");
-    var personTotal = document.querySelectorAll(".person-pay");
-    personTotal.forEach(ele => ele.style.display = "none")
-    total.style.display = "none"
-    if (bill >= 0 && tip >= 0) {
-        var totalBill = bill + tip / 100 * bill;
+let calculate = () => {
+    let bill = parseFloat(document.querySelector("#bill").value);
+    let tip = parseFloat(document.querySelector("#tip").value);
+    if (isNaN(bill) || bill < 0) alerting(document.querySelector(".bill-alert"))
+    else if (isNaN(tip) || tip < 0) alerting(document.querySelector(".tip-alert"))
+    else {
+        let total = document.querySelector("#pay");
+        let personTotal = document.querySelectorAll(".person-pay");
+        personTotal.forEach(ele => ele.style.display = "none")
+        total.style.display = "none"
+        let totalBill = bill + tip / 100 * bill;
         if (box.checked) {
             if (Number.isInteger(parseFloat(split.value)) && split.value >= 1) {
-                console.log(split)
                 document.querySelector(".person-bill").innerHTML = `₹` + (totalBill / split.value).toFixed(2)
                 document.querySelector(".person-tip").innerHTML = `₹` + ((totalBill - bill) / split.value).toFixed(2)
                 document.querySelector(".total-bill").innerHTML = `₹` + totalBill.toFixed(2)
@@ -29,13 +30,10 @@ function calculate() {
             document.querySelector(".total-tip").innerHTML = `₹` + (totalBill - bill).toFixed(2)
             total.style.display = "block"
         }
-
     }
-    else if (bill < 0) alerting(document.querySelector(".bill-alert"))
-    else if (tip < 0) alerting(document.querySelector(".tip-alert"))
 }
 
-function check() {
+let checkBox = () => {
     if (box.checked) {
         split.disabled = false
         split.value = "2"
@@ -46,9 +44,11 @@ function check() {
     }
 }
 
-function alerting(alert) {
+let alerting = (alert) => {
     alert.style.display = "block"
     setTimeout(function () { alert.style.display = "none" }, 3000)
 }
 
+calcButton.addEventListener("click", calculate);
+splitButton.addEventListener("click", checkBox);
 
